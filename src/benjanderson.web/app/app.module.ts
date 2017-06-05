61903dffcf65ea74b16a5c5c1872615831de99ed
Ben Anderson <ben.anderson@healthcatalyst.com>
2017-06-04 19:07:39 -0600

    added color test and nav

--- src/benjanderson.web/app/app.module.ts
+++ src/benjanderson.web/app/app.module.ts
@@ -7,12 +7,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ColorTestComponent } from './color-test/color-test.component';
 import { HomeComponent } from './home/home.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 @NgModule({
     declarations: [
         AppComponent,
         ColorTestComponent,
-        HomeComponent
+        HomeComponent,
+        NavBarComponent
     ],
     imports: [
         BrowserModule,
