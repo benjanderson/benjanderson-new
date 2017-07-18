32f3146b72f45f4a4ea3d9b1096ac6f7a5881e2b
Ben Anderson <ben.anderson@healthcatalyst.com>
2017-07-18 13:22:18 -0600

    first rough draft of about page

--- src/benjanderson.web/app/about-me/about-me.component.ts
+++ src/benjanderson.web/app/about-me/about-me.component.ts
@@ -7,6 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutMeComponent implements OnInit {
 
+  public history = [
+    {
+      title: 'Health Catalyst',
+      subtitle: 'Senior Software Engineer',
+      description: `Led team of 3 engineers and 1 quality assurance to build enterprise web application. 
+      Built with Angular, asp.net, c#, and sql server.`,
+      date: 'Oct 2014 - Present',
+      icon: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg'
+    },
+    {
+      title: 'eForce Software',
+      subtitle: 'Software Architect',
+      description: `Plan and Oversee overall software structure and maintain code quality`,
+      date: 'Aug 2012 - Oct 2014',
+      icon: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg'
+    },
+    {
+      title: 'eForce Software',
+      subtitle: 'Software Engineer',
+      description: `Design and maintain web based law enforcement software for clients around the US`,
+      date: 'Mar 2007 - Aug 2012',
+      icon: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg'
+    },
+    {
+      title: 'Utah State University',
+      subtitle: 'Bachelor of Science (BS), Computer Science',
+      description: ``,
+      date: '2003 - 2010',
+      icon: 'https://s3-us-west-2.amazonaws.com/s.cdpn.io/148866/cd-icon-picture.svg'
+    },
+  ];
+
+  public currentYear = new Date().getFullYear();
+
   constructor() { }
 
   ngOnInit() {
