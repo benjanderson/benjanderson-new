cb49c4e6d84496709626a6a76acdeaf74259f15a
Ben Anderson <ben.anderson@healthcatalyst.com>
2017-12-04 08:54:55 -0700

    intial chess board and pieces very very rough

--- src/app/chess/chess.component.ts
+++ src/app/chess/chess.component.ts
@@ -7,19 +7,63 @@ import { Chess, ChessMove } from 'chess.js';
   styleUrls: ['./chess.component.scss']
 })
 export class ChessComponent implements OnInit {
+  public chess: Chess;
 
-  constructor() { }
+  constructor() {
+    this.chess = new (<any>window).Chess();
+  }
 
   ngOnInit() {
-    const chess: Chess = new (<any>window).Chess();
-
-    while (!chess.game_over()) {
-      const moves: ChessMove[] = chess.moves();
+    console.log(this.chess.SQUARES);
+    while (!this.chess.game_over()) {
+      const moves: ChessMove[] = this.chess.moves();
       const move: ChessMove = moves[Math.floor(Math.random() * moves.length)];
-      console.log(move);
-      chess.move(move);
+      this.chess.move(move);
     }
-    console.log(chess.pgn());
+    console.log(this.chess.pgn());
+
+    interact('.draggable')
+      .draggable({
+        // enable inertial throwing
+        inertia: true,
+        // keep the element within the area of it's parent
+        restrict: {
+          // restriction: 'parent',
+          endOnly: true,
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+        // enable autoScroll
+        autoScroll: true,
+
+        // call this function on every dragmove event
+        onmove: this.dragMoveListener,
+        // call this function on every dragend event
+        // onend: function (event) {
+        //   let textEl = event.target.querySelector('p');
+
+        //   textEl && (textEl.textContent =
+        //     'moved a distance of '
+        //     + (Math.sqrt(event.dx * event.dx +
+        //       event.dy * event.dy) | 0) + 'px');
+        // }
+      });
   }
+
+  public dragMoveListener(event) {
+    const target = event.target,
+      // keep the dragged position in the data-x/data-y attributes
+      x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+      y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+    // translate the element
+    target.style.webkitTransform =
+      target.style.transform =
+      'translate(' + x + 'px, ' + y + 'px)';
+
+    // update the posiion attributes
+    target.setAttribute('data-x', x);
+    target.setAttribute('data-y', y);
+  }
+
 
 }
